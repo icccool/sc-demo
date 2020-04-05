27f52e9aa5d6b270f47efe77d5da65e7c4d22634
wang <wangjibo@hydee.cn>
2020-04-05 11:13:11 +0800

    init

---
+++ src/main/java/com/scdemo/ScDemoApplication.java
@@ -0,0 +1,13 @@
+package com.scdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScDemoApplication.class, args);
+    }
+
+}
