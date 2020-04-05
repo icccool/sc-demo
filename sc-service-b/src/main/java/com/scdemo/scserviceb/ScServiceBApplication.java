fbe710d69913977b7bd1d6ca050366fb1066c449
wang <wangjibo@hydee.cn>
2020-04-05 11:53:41 +0800

    init2

---
+++ sc-service-b/src/main/java/com/scdemo/scserviceb/ScServiceBApplication.java
@@ -0,0 +1,13 @@
+package com.scdemo.scserviceb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScServiceBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScServiceBApplication.class, args);
+    }
+
+}
---
+++ sc-service-b/src/test/java/com/scdemo/scserviceb/ScServiceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scdemo.scserviceb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScServiceBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
