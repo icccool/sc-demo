0c521476cdfae7d052359e0da3769700e728f120
WangJibo <2880985654@qq.com>
2020-04-08 20:14:00 +0800

    fix: 模块调整

---
+++ sc-service-eureka/src/main/java/com/scdemo/scservicea/ScServiceAApplication.java
@@ -0,0 +1,13 @@
+package com.scdemo.scservicea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScServiceAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScServiceAApplication.class, args);
+	}
+
+}
